c622c48c26fa8accf8b1aa71a084aff91e807e05
Tomy Devogelaere <tomy.devogelaere@telenet.be>
2026-04-24 11:14:55 +0200

    moved main background from admin pages to the layout

--- app/(logged-in)/layout.tsx
+++ app/(logged-in)/layout.tsx
@@ -16,22 +16,13 @@ export default async function LoggedInLayout({
     }
 
     return (
-        <div className="min-h-screen flex flex-col">
-            <nav className="bg-gray-200 flex justify-between p-4 items-center">
-                <ul className="flex gap-4">
-                    <li>
-                        <Link href="/my-account">My Account</Link>
-                    </li>
-                    <li>
-                        <Link href="/change-password">Change Password</Link>
-                    </li>
-                </ul>
-                <div>
-                    <LogoutButton />
-                </div>
-
-            </nav>
-            <div className="flex-1 flex justify-center items-center">{children}</div>
-        </div>
+        <main className="relative flex justify-center items-center min-h-screen overflow-hidden p-4 font-sans">
+            {/* Achtergrond laag */}
+            <div className="absolute inset-0 -z-10 bg-slate-50 dark:bg-black font-sans">
+                <div className="absolute inset-0 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+                <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-primary/10 opacity-20 blur-[100px]"></div>
+            </div>
+            {children}
+        </main>
     );
 }
--- app/(logged-out)/password-reset/page.tsx
+++ app/(logged-out)/password-reset/page.tsx
@@ -34,15 +34,6 @@ export default function PasswordReset() {
 
 
     return (
-        <main className="relative flex justify-center items-center min-h-screen overflow-hidden p-4 font-sans">
-            {/* Achtergrond laag */}
-            <div className="absolute inset-0 -z-10 bg-slate-50 dark:bg-black font-sans">
-                <div
-                    className="absolute inset-0 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
-                <div
-                    className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-primary/10 opacity-20 blur-[100px]"></div>
-            </div>
-
             <AnimatePresence mode="wait">
                 <motion.div
                     key="form"
@@ -158,6 +149,5 @@ export default function PasswordReset() {
                 </motion.div>
 
             </AnimatePresence>
-        </main>
     )
 }
